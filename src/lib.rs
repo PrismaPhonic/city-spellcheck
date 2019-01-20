ee491f6ef9688f347f60b6f32d885a076fd5146d
Peter Farr <Peter@PrismaPhonic.com>
2019-01-19 16:37:23 -0800

    Added in type safety for regions (states, territories etc.) and changed CityData to match region to states

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ use rayon::iter::{IntoParallelIterator, ParallelIterator};
 pub struct CityData {
     pub names: Vec<String>,
     pub countries: Vec<Country>,
-    pub regions: Vec<String>,
+    pub regions: Vec<Region>,
     pub latitudes: Vec<f32>,
     pub longitudes: Vec<f32>,
 }
@@ -36,6 +36,67 @@ pub struct CityData {
 pub enum Country {
     US,
     CA,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum Region {
+    Province,
+    Territory,
+    States(State),
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum State {
+    AL,
+    AK,
+    AZ,
+    AR,
+    CA,
+    CO,
+    CT,
+    DE,
+    FL,
+    GA,
+    HI,
+    ID,
+    IL,
+    IN,
+    IA,
+    KS,
+    KY,
+    LA,
+    ME,
+    MD,
+    MA,
+    MI,
+    MN,
+    MS,
+    MO,
+    MT,
+    NE,
+    NV,
+    NH,
+    NJ,
+    NM,
+    NY,
+    NC,
+    ND,
+    OH,
+    OK,
+    OR,
+    PA,
+    RI,
+    SC,
+    SD,
+    TN,
+    TX,
+    UT,
+    VT,
+    VA,
+    WA,
+    WV,
+    WI,
+    WY,
 }
 
 impl fmt::Display for Country {
@@ -47,11 +108,69 @@ impl fmt::Display for Country {
     }
 }
 
+impl fmt::Display for Region {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Region::States(State::AL) => write!(f, "AL"),
+            Region::States(State::AK) => write!(f, "AK"),
+            Region::States(State::AZ) => write!(f, "AZ"),
+            Region::States(State::AR) => write!(f, "AR"),
+            Region::States(State::CA) => write!(f, "CA"),
+            Region::States(State::CO) => write!(f, "CO"),
+            Region::States(State::CT) => write!(f, "CT"),
+            Region::States(State::DE) => write!(f, "DE"),
+            Region::States(State::FL) => write!(f, "FL"),
+            Region::States(State::GA) => write!(f, "GA"),
+            Region::States(State::HI) => write!(f, "HI"),
+            Region::States(State::ID) => write!(f, "ID"),
+            Region::States(State::IL) => write!(f, "IL"),
+            Region::States(State::IN) => write!(f, "IN"),
+            Region::States(State::IA) => write!(f, "IA"),
+            Region::States(State::KS) => write!(f, "KS"),
+            Region::States(State::KY) => write!(f, "KY"),
+            Region::States(State::LA) => write!(f, "LA"),
+            Region::States(State::ME) => write!(f, "ME"),
+            Region::States(State::MD) => write!(f, "MD"),
+            Region::States(State::MA) => write!(f, "MA"),
+            Region::States(State::MI) => write!(f, "MI"),
+            Region::States(State::MN) => write!(f, "MN"),
+            Region::States(State::MS) => write!(f, "MS"),
+            Region::States(State::MO) => write!(f, "MO"),
+            Region::States(State::MT) => write!(f, "MT"),
+            Region::States(State::NE) => write!(f, "NE"),
+            Region::States(State::NV) => write!(f, "NV"),
+            Region::States(State::NH) => write!(f, "NH"),
+            Region::States(State::NJ) => write!(f, "NJ"),
+            Region::States(State::NM) => write!(f, "NM"),
+            Region::States(State::NY) => write!(f, "NY"),
+            Region::States(State::NC) => write!(f, "NC"),
+            Region::States(State::ND) => write!(f, "ND"),
+            Region::States(State::OH) => write!(f, "OH"),
+            Region::States(State::OK) => write!(f, "OK"),
+            Region::States(State::OR) => write!(f, "OR"),
+            Region::States(State::PA) => write!(f, "PA"),
+            Region::States(State::RI) => write!(f, "RI"),
+            Region::States(State::SC) => write!(f, "SC"),
+            Region::States(State::SD) => write!(f, "SD"),
+            Region::States(State::TN) => write!(f, "TN"),
+            Region::States(State::TX) => write!(f, "TX"),
+            Region::States(State::UT) => write!(f, "UT"),
+            Region::States(State::VT) => write!(f, "VT"),
+            Region::States(State::VA) => write!(f, "VA"),
+            Region::States(State::WA) => write!(f, "WA"),
+            Region::States(State::WV) => write!(f, "WV"),
+            Region::States(State::WI) => write!(f, "WI"),
+            Region::States(State::WY) => write!(f, "WY"),
+            _ => write!(f, ""),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct City<'a> {
     name: &'a str,
     country: Country,
-    region: &'a str,
+    region: Region,
     latitude: f32,
     longitude: f32,
 }
@@ -129,6 +248,11 @@ impl CityData {
                     _ => continue,
                 };
 
+                let region = match country {
+                    Country::US => CityData::state_match(region),
+                    Country::CA => Region::Territory,
+                };
+
                 self.add_city(name, country, region, latitude, longitude);
             };
         }
@@ -136,57 +260,73 @@ impl CityData {
         Ok(())
     }
 
-    // /// Syncs to redis at default internal ip address
-    // /// Todo: Update to accept any ip addr
-    // pub fn sync_to_redis(&self) -> Result<(), Box<dyn Error>> {
-    //     // connect to redis
-    //     let client = redis::Client::open("redis://127.0.0.1/")?;
-    //     let con = client.get_connection()?;
+    fn state_match(region: &str) -> Region {
+        match region {
+            "AL" => Region::States(State::AL),
+            "AK" => Region::States(State::AK),
+            "AZ" => Region::States(State::AZ),
+            "AR" => Region::States(State::AR),
+            "CA" => Region::States(State::CA),
+            "CO" => Region::States(State::CO),
+            "CT" => Region::States(State::CT),
+            "DE" => Region::States(State::DE),
+            "FL" => Region::States(State::FL),
+            "GA" => Region::States(State::GA),
+            "HI" => Region::States(State::HI),
+            "ID" => Region::States(State::ID),
+            "IL" => Region::States(State::IL),
+            "IN" => Region::States(State::IN),
+            "IA" => Region::States(State::IA),
+            "KS" => Region::States(State::KS),
+            "KY" => Region::States(State::KY),
+            "LA" => Region::States(State::LA),
+            "ME" => Region::States(State::ME),
+            "MD" => Region::States(State::MD),
+            "MA" => Region::States(State::MA),
+            "MI" => Region::States(State::MI),
+            "MN" => Region::States(State::MN),
+            "MS" => Region::States(State::MS),
+            "MO" => Region::States(State::MO),
+            "MT" => Region::States(State::MT),
+            "NE" => Region::States(State::NE),
+            "NV" => Region::States(State::NV),
+            "NH" => Region::States(State::NH),
+            "NJ" => Region::States(State::NJ),
+            "NM" => Region::States(State::NM),
+            "NY" => Region::States(State::NY),
+            "NC" => Region::States(State::NC),
+            "ND" => Region::States(State::ND),
+            "OH" => Region::States(State::OH),
+            "OK" => Region::States(State::OK),
+            "OR" => Region::States(State::OR),
+            "PA" => Region::States(State::PA),
+            "RI" => Region::States(State::RI),
+            "SC" => Region::States(State::SC),
+            "SD" => Region::States(State::SD),
+            "TN" => Region::States(State::TN),
+            "TX" => Region::States(State::TX),
+            "UT" => Region::States(State::UT),
+            "VT" => Region::States(State::VT),
+            "VA" => Region::States(State::VA),
+            "WA" => Region::States(State::WA),
+            "WV" => Region::States(State::WV),
+            "WI" => Region::States(State::WI),
+            "WY" => Region::States(State::WY),
+            _ => Region::Territory,
+        }
+    }
 
-    //     // throw away the result, just make sure it does not fail
-    //     let _: () = con.del("city-names")?;
-    //     let _: () = con.lpush("city-names", self.names.clone())?;
-    //     let _: () = con.del("city-countries")?;
-    //     let _: () = con.lpush("city-countries", self.countries.clone())?;
-    //     let _: () = con.del("city-regions")?;
-    //     let _: () = con.lpush("city-regions", self.regions.clone())?;
-    //     let _: () = con.del("city-latitudes")?;
-    //     let _: () = con.lpush("city-latitudes", self.latitudes.clone())?;
-    //     let _: () = con.del("city-longitudes")?;
-    //     let _: () = con.lpush("city-longitudes", self.longitudes.clone())?;
-
-    //     Ok(())
-    // }
-
-    // pub fn populate_from_redis(&mut self) -> Result<(), Box<dyn Error>> {
-    //     // connect to redis
-    //     let client = redis::Client::open("redis://127.0.0.1/")?;
-    //     let con = client.get_connection()?;
-
-    //     // get vectors from redis for cities data
-    //     let names: Vec<String> = con.lrange("city-names", 0, con.llen("city-names").unwrap())?;
-    //     let countries: Vec<String> =
-    //         con.lrange("city-countries", 0, con.llen("city-countries").unwrap())?;
-    //     let regions: Vec<String> =
-    //         con.lrange("city-regions", 0, con.llen("city-regions").unwrap())?;
-    //     let latitudes: Vec<f32> =
-    //         con.lrange("city-latitudes", 0, con.llen("city-latitudes").unwrap())?;
-    //     let longitudes: Vec<f32> =
-    //         con.lrange("city-longitudes", 0, con.llen("city-longitudes").unwrap())?;
-
-    //     self.names = names;
-    //     self.countries = countries;
-    //     self.regions = regions;
-    //     self.latitudes = latitudes;
-    //     self.longitudes = longitudes;
-
-    //     Ok(())
-    // }
-
-    fn add_city(&mut self, name: &str, country: Country, region: &str, latitude: f32, longitude: f32) {
+    fn add_city(
+        &mut self,
+        name: &str,
+        country: Country,
+        region: Region,
+        latitude: f32,
+        longitude: f32,
+    ) {
         self.names.push(name.to_string());
         self.countries.push(country);
-        self.regions.push(region.to_string());
+        self.regions.push(region);
         self.latitudes.push(latitude);
         self.longitudes.push(longitude);
     }
@@ -195,7 +335,7 @@ impl CityData {
         City {
             name: &self.names[idx],
             country: self.countries[idx],
-            region: &self.regions[idx],
+            region: self.regions[idx],
             latitude: self.latitudes[idx],
             longitude: self.longitudes[idx],
         }
@@ -212,7 +352,7 @@ impl CityData {
             longitude,
         };
 
-        let str_dist = sift4_simple(city, term) as f32;
+        let str_dist = sift4(city, term) as f32;
         let mut str_score = if str_dist >= term.len() as f32 {
             0.0
         } else {
@@ -296,15 +436,27 @@ mod tests {
     #[test]
     fn test_citydata_struct_nyc() {
         let mut cities = CityData::new();
-        cities.add_city("New York City", Country::US, "NY", 40.7128, 74.0060);
-        assert_eq!(format!("{:?}", cities.get_city(0)), "City { name: \"New York City\", country: US, region: \"NY\", latitude: 40.7128, longitude: 74.006 }");
+        cities.add_city(
+            "New York City",
+            Country::US,
+            Region::States(State::NY),
+            40.7128,
+            74.0060,
+        );
+        assert_eq!(format!("{:?}", cities.get_city(0)), "City { name: \"New York City\", country: US, region: States(NY), latitude: 40.7128, longitude: 74.006 }");
     }
 
     #[test]
     fn test_citydata_struct_sf() {
         let mut cities = CityData::new();
-        cities.add_city("San Francisco", Country::US, "CA", 37.7749, 122.4194);
-        assert_eq!(format!("{:?}", cities.get_city(0)), "City { name: \"San Francisco\", country: US, region: \"CA\", latitude: 37.7749, longitude: 122.4194 }");
+        cities.add_city(
+            "San Francisco",
+            Country::US,
+            Region::States(State::CA),
+            37.7749,
+            122.4194,
+        );
+        assert_eq!(format!("{:?}", cities.get_city(0)), "City { name: \"San Francisco\", country: US, region: States(CA), latitude: 37.7749, longitude: 122.4194 }");
     }
 
     #[test]
@@ -313,12 +465,15 @@ mod tests {
         cities
             .populate_from_file("data/cities_canada-usa-filtered.csv")
             .unwrap();
-        assert_eq!(format!("{:?}", cities.get_city(0)), "City { name: \"Abbotsford\", country: CA, region: \"02\", latitude: 49.05798, longitude: -122.25257 }");
+        assert_eq!(
+            format!("{:?}", cities.get_city(0)),
+            "City { name: \"Abbotsford\", country: CA, region: Territory, latitude: 49.05798, longitude: -122.25257 }"
+        );
     }
 
     #[test]
     fn test_str_dist() {
-        assert_eq!(sift4_simple("Londo", "London"), 1);
+        assert_eq!(sift4("Londo", "London"), 1);
     }
 
     #[test]
@@ -359,25 +514,9 @@ mod tests {
             longitude: -81.23304,
         };
         let results = cities.search("London", Some(london));
-        assert_eq!(format!("{:?}", results),"[FuzzyResult { city: \"London, 08, CA\", latitude: 42.98339, longitude: -81.23304, score: 1.0 }, FuzzyResult { city: \"London, OH, US\", latitude: 39.88645, longitude: -83.44825, score: 0.6252391 }, FuzzyResult { city: \"London, KY, US\", latitude: 37.12898, longitude: -84.08326, score: 0.6250727 }, FuzzyResult { city: \"Lemont, IL, US\", latitude: 41.67364, longitude: -88.00173, score: 0.52094036 }, FuzzyResult { city: \"Brant, 08, CA\", latitude: 43.1334, longitude: -80.34967, score: 0.5208334 }]");
+        assert_eq!(
+            format!("{:?}", results),
+            "[FuzzyResult { city: \"London, , CA\", latitude: 42.98339, longitude: -81.23304, score: 1.0 }, FuzzyResult { city: \"London, OH, US\", latitude: 39.88645, longitude: -83.44825, score: 0.6252391 }, FuzzyResult { city: \"London, KY, US\", latitude: 37.12898, longitude: -84.08326, score: 0.6250727 }, FuzzyResult { city: \"Lemont, IL, US\", latitude: 41.67364, longitude: -88.00173, score: 0.52094036 }, FuzzyResult { city: \"Brant, , CA\", latitude: 43.1334, longitude: -80.34967, score: 0.5208334 }]"
+        );
     }
-
-    // #[test]
-    // fn push_and_pull_redis() {
-    //     let mut cities_first = CityData::new();
-    //     cities_first
-    //         .populate_from_file("data/cities_canada-usa-filtered.csv")
-    //         .unwrap();
-    //     cities_first.sync_to_redis().unwrap();
-
-    //     let mut cities = CityData::new();
-    //     cities.populate_from_redis().unwrap();
-
-    //     let london = Coordinate {
-    //         latitude: 42.98339,
-    //         longitude: -81.23304,
-    //     };
-    //     let results = cities.search("London", Some(london));
-    //     assert_eq!(format!("{:?}", results),"[FuzzyResult { city: \"London, 08, CA\", latitude: 42.98339, longitude: -81.23304, score: 1.0 }, FuzzyResult { city: \"London, OH, US\", latitude: 39.88645, longitude: -83.44825, score: 0.6252391 }, FuzzyResult { city: \"London, KY, US\", latitude: 37.12898, longitude: -84.08326, score: 0.6250727 }, FuzzyResult { city: \"Lemont, IL, US\", latitude: 41.67364, longitude: -88.00173, score: 0.52094036 }, FuzzyResult { city: \"Brant, 08, CA\", latitude: 43.1334, longitude: -80.34967, score: 0.5208334 }]");
-    // }
 }
